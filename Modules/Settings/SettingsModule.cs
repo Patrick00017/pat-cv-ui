44518b520501a5157cd7e5cfefa47db458a66e18
patrick <742679653@qq.com>
2025-12-21 19:00:44 +0800

    feat: add settings module.

---
+++ Modules/Settings/SettingsModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Settings.Views;
+
+namespace Settings
+{
+    public class SettingsModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
